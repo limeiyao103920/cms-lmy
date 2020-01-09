5797449737c74fa26fdfe0e134531da548394580
limeiyao <71733035@qq.com>
2020-01-09 09:51:34 +0800

    dao结束

---
+++ src/main/java/com/maven/web/dao/CategoryMapper.java
@@ -0,0 +1,20 @@
+package com.maven.web.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.maven.web.pojo.User;
+
+public interface CategoryMapper {
+
+	List<User> select(User user);
+	
+	User selectById(Integer id);
+	
+	int insert(User user);
+	
+	int updata(User user);
+	
+	int delete(@Param("ids") String ids);
+}
